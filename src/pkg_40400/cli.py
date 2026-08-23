566c020958a4f0e5ae32f43a6bcf2cd026b3eeed
Andre Choi <andreclc@gmail.com>
2026-08-23 05:54:54 -0700

    update logger

--- src/pkg_40400/cli.py
+++ src/pkg_40400/cli.py
@@ -1,30 +1,15 @@
 """
-Test Logging and Decorator
+Project Script
 """
 
 from pkg_40400 import (
-    ClsFalseError,
     Logger,
-    raise_on_false,
     version,
 )
 
 
-@raise_on_false(exception_type=ClsFalseError, custom_message="")
-def try_decorator(enable_log: bool = False) -> bool:
-    """
-    Return False Intentionally To Test Decorator
-    """
-    return False
-
-
-def main() -> bool:
+def main() -> None:
     Logger.info(f"{version}")
-
-    try:
-        return try_decorator(enable_log=True)
-    except ClsFalseError:
-        return False
 
 
 if __name__ == "__main__":
